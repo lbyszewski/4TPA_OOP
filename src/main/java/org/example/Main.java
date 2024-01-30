caa2173ddc046627ccc675a12845672e63476a8b
inz Lukasz Byszewski <gdx153483@wsb.gda.pl>
2024-01-30 15:25:14 +0100

    Files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+        /*
+
+                1. Implementacja programu w którym użytkownik wprowadzi dwie zmienne
+                    określające przedział a następnie dzielnik na podstawie którego
+                     sprawdzi podzielność przedziału
+
+                     List kroków
+                        - output  msg
+                        - Scanner
+                        - loop
+                            - devide - input
+                            - output - msg and value
+
+
+
+
+
+
+                2.  Implementacja programu tabliczka mnożenia
+                3. Implementacja programu  - rekurencja  ciągu fibb i silnii
+                4. Zadania dodatkowe z pętli for do realizacji na zajęciach  - wykonać min 6 pełnych zadań
+
+
+
+
+
+
+
+
+
+
+
+
+         */
+
+    }
+}
