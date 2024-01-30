49d81b40ff95a676cbd0cadaf6bc2cd5ad283900
inz Lukasz Byszewski <gdx153483@wsb.gda.pl>
2024-01-30 16:59:50 +0100

    Files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,6 +17,7 @@ public class Main {
                         - Scanner
                         - loop
                             - devide - input
+                            - veryfication
                             - output - msg and value
 
 
